175da0c4e1204fba40a1996343693ac6fc04e900
Geonu Yu <dbrjdn2051@naver.com[C>
2024-10-31 23:22:14 +0900

    [refactor] (RestClientService) : Map 객체 타입 명시, restClient 제대로 활용

--- src/main/java/io/security/oauth2/oauth2provider/service/RestClientService.java
+++ src/main/java/io/security/oauth2/oauth2provider/service/RestClientService.java
@@ -1,8 +1,9 @@
 package io.security.oauth2.oauth2provider.service;
 
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Service;
 import org.springframework.util.LinkedMultiValueMap;
@@ -17,20 +18,23 @@ import java.util.Map;
 public class RestClientService {
 
     public String exchangeCodeForToken(String tokenUri, LinkedMultiValueMap<String, String> body) {
-        HttpHeaders headers = new HttpHeaders();
-        headers.setContentType(MediaType.APPLICATION_FORM_URLENCODED);
-        URI uri = URI.create(tokenUri);
-
-        Map response = RestClient.create()
+        Map<String, String> response = RestClient.create()
                 .post()
-                .uri(uri)
-                .headers(httpHeaders -> httpHeaders.addAll(headers))
+                .uri(URI.create(tokenUri))
+                .headers(httpHeaders -> httpHeaders.setContentType(MediaType.APPLICATION_FORM_URLENCODED))
                 .body(body)
-                .retrieve()
-                .body(Map.class);
+                .accept(MediaType.APPLICATION_JSON)
+                .exchange((clientRequest, clientResponse) -> {
+                    if (!clientResponse.getStatusCode().is4xxClientError()) {
+                        return new ObjectMapper()
+                                .readValue(clientResponse.getBody(),
+                                        new TypeReference<Map<String, String>>() {});
+                    }
+                    throw new IllegalArgumentException();
+                });
 
         log.info("Response = {}", response);
-        return (String) response.get("access_token");
+        return response.get("access_token");
     }
 
 }
